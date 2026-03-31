ae5322cab3250fbcf5d1b5ee94b9d6452686c163
ramazanov-am <ramazanov-am@zvuk.com>
2026-03-31 10:20:17 +0300

    Using external metadata database

--- airflow_configuring.sh
+++ airflow_configuring.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 NAMESPACE="airflow"
-GITSYNC_USERNAME="airflow_dags"
-GITSYNC_PASSWORD="glpat-G1BZ505hVyVKwIWkcibeY286MQp1OjUH.01.0w0ga8nvd"
+# gitSync
+GITSYNC_USERNAME="airflow"
+GITSYNC_PASSWORD="glpat-vk6-ev2hmQDKQGV2avl95W86MQp1OjQH.01.0w0hkv4mr"
+# airflow metadata
+PSQL_COMMAND="psql -h postgresql.postgresql.svc.cluster.local -p 5432 -U postgres"
+USERNAME="airflow"
+PASSWORD="airflow"
+DATABASE="airflow"
 
 
 kubectl create namespace ${NAMESPACE} --dry-run=client -o yaml | kubectl apply -f -
@@ -12,4 +18,19 @@ kubectl create secret generic git-credentials -n ${NAMESPACE} \
     --from-literal=GIT_SYNC_PASSWORD=${GITSYNC_PASSWORD} \
     --from-literal=GITSYNC_USERNAME=${GITSYNC_USERNAME} \
     --from-literal=GITSYNC_PASSWORD=${GITSYNC_PASSWORD} \
+    --dry-run=client -o yaml | kubectl apply -f -
+
+# Прежде всего в postgresql должны быть созданы user и database
+kubectl run -it -n ${NAMESPACE} --rm psql \
+  --image=bitnamilegacy/postgresql:16.1.0-debian-11-r25 \
+  --env=PGPASSWORD=postgres \
+  --restart=Never \
+  --command -- /bin/sh -c "\
+  ${PSQL_COMMAND} -d postgres -c \"SELECT create_user_if_not_exists('${USERNAME}', '${PASSWORD}');\"
+  # Создать database, если не существует (https://stackoverflow.com/a/18389184)
+  echo \"SELECT 'CREATE DATABASE ${DATABASE} OWNER ${USERNAME}' WHERE NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = '${DATABASE}')\gexec\" | \
+  ${PSQL_COMMAND} -d postgres"
+
+kubectl create secret generic external-postgresql-secret -n ${NAMESPACE} \
+    --from-literal=connection=postgresql://${USERNAME}:${PASSWORD}@postgresql.postgresql.svc.cluster.local:5432/${DATABASE} \
     --dry-run=client -o yaml | kubectl apply -f -
