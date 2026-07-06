089fb4d56ed945ba38f9e63cb3034d41a186d753
ramazanov-am <ramalimav@gmail.com>
2026-07-06 18:10:31 +0300

    Using hostPath instead of pvc

--- jupyterhub/install.sh
+++ jupyterhub/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-helm install jupyterhub . -n jupyterhub -f values-minimum.yaml
+helm install jupyterhub . -n jupyterhub -f values-minimum.yaml --set singleuser.storage.extraVolumes[0].hostPath.path=$(pwd)/notebooks
