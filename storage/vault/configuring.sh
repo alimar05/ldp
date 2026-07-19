b23435d459e9f219cb6f6a8036e71e3ef797d9cd
ramazanov-am <ramalimav@gmail.com>
2026-07-19 20:45:20 +0300

    SSO for vault

--- storage/vault/configuring.sh
+++ storage/vault/configuring.sh
@@ -5,6 +5,8 @@ VAULT_ROOT_TOKEN="hvs.FsNWzPEjvR6OzFsAuohVzm85"
 VAULT_UNSEAL_KEY_1="w5Md279toLSyDbTll6Vt2vP4ZsxS2kwoE1SNcZuBRU5w"
 VAULT_UNSEAL_KEY_2="DB8l67GJ+yt5E8i+AHQWTYmDNHjXfi1CKm2U/ywW3o7L"
 VAULT_UNSEAL_KEY_3="li9DwmE/9RCX6OGUY3XnjOXMlOKo3rHzyrDgm+GRj642"
+# Get from Keycloak UI
+KEYCLOAK_CLIENT_SECRET="sHKgbqEZdkCLQITNLTcppi899xuC2EKP"
 
 
 kubectl create secret generic vault-root-token -n ${NAMESPACE} \
@@ -15,4 +17,8 @@ kubectl create secret generic vault-unseal-keys -n ${NAMESPACE} \
     --from-literal=VAULT_UNSEAL_KEY_1=${VAULT_UNSEAL_KEY_1} \
     --from-literal=VAULT_UNSEAL_KEY_2=${VAULT_UNSEAL_KEY_2} \
     --from-literal=VAULT_UNSEAL_KEY_3=${VAULT_UNSEAL_KEY_3} \
+    --dry-run=client -o yaml | kubectl apply -f -
+
+kubectl create secret generic vault-keycloak-client-secret -n ${NAMESPACE} \
+    --from-literal=KEYCLOAK_CLIENT_SECRET=${KEYCLOAK_CLIENT_SECRET} \
     --dry-run=client -o yaml | kubectl apply -f -
