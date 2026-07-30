71780f0663dfa3ec151772d5ff09888d14e5c825
ramazanov-am <ramalimav@gmail.com>
2026-07-30 12:38:21 +0300

    Refactoring

---
+++ utils/nginx-ingress/configuring.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+kubectl apply -f global-configuration.yaml
+kubectl apply -f postgresql-transport.yaml
--- utils/nginx-ingress/install.sh
+++ utils/nginx-ingress/install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-helm install nginx-ingress nginx-ingress -n nginx-ingress -f values-minimum.yaml
+source configuring.sh
+
+helm install nginx-ingress nginx-ingress -n nginx-ingress --create-namespace -f values-minimum.yaml
