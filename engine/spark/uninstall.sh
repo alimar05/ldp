39696bee4950f4f53e6341bef6638d9c5d4d21b0
ramazanov-am <ramalimav@gmail.com>
2026-09-25 14:17:29 +0300

    Using webhook for spark operator

--- engine/spark/uninstall.sh
+++ engine/spark/uninstall.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# set -e
+
 helm uninstall spark -n spark
+kubectl delete secrets -n spark spark-spark-operator-webhook-certs
+kubectl delete crd -n spark sparkapplications.sparkoperator.k8s.io sparkconnects.sparkoperator.k8s.io
